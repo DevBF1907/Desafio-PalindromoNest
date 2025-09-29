79e4a10ca3334b5822931e11c27083ebfb592675
Brenno Felipe <devbrenno1907@gmaail.com>
2025-09-29 20:16:41 -0300

    add documemtation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,44 @@
 /* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Palindrome API')
+    .setDescription(`
+# API de Verificação de Palíndromos
+
+Esta API verifica se um número é um palíndromo, ou seja, 
+se é igual quando lido da esquerda para direita e vice-versa.
+
+## Exemplos de uso:
+- ✅ 121 → true
+- ❌ 123 → false
+- ❌ -121 → false
+- ❌ 10 → false
+    `)
+    .setVersion('1.0')
+    .setContact('Seu Nome', 'https://seusite.com', 'seu@email.com')
+    .addTag('palindrome', 'Operações relacionadas a palíndromos')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document, {
+    customSiteTitle: 'Palindrome API Documentation',
+    customCss: `
+      .swagger-ui .topbar { display: none }
+      .swagger-ui .info { margin: 20px 0 }
+    `,
+    customfavIcon: '/favicon.ico'
+  });
+
+  
+  await app.listen(3000);
+  console.log('🚀 Application running on: http://localhost:3000');
+  console.log('📚 Swagger documentation: http://localhost:3000/api');
 }
 bootstrap();
--- src/palindrome/palindrome.controller.ts
+++ src/palindrome/palindrome.controller.ts
@@ -2,13 +2,35 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PalindromeService } from './palindrome.service';
 import { PalindromeResponseDto } from './dto/palindrome-reponse.dto';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags('palindrome')
 @Controller()
 export class PalindromeController {
   
   constructor(private readonly palindromeService: PalindromeService) {}
 
   @Get('palindrome')
+    @ApiOperation({ 
+    summary: 'Verifica se um número é palíndromo',
+    description: 'Retorna true se o número for igual quando lido de trás para frente'
+  })
+  @ApiQuery({
+    name: 'num',
+    required: true,
+    description: 'Número a ser verificado',
+    example: '121'
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Número verificado com sucesso',
+    type: PalindromeResponseDto
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Parâmetro inválido ou não informado'
+  })
   checkPalindrome(@Query('num') num: string): PalindromeResponseDto {
     return this.palindromeService.checkPalindrome(num);
   }
